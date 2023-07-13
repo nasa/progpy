1a445088b1679597cc10d594e24eead107bc652b
Christopher Teubert <christopher.a.teubert@nasa.gov>
2023-07-13 11:55:24 -0700

    Add test for auto time step

--- tests/test_base_models.py
+++ tests/test_base_models.py
@@ -920,7 +920,11 @@ class TestModels(unittest.TestCase):
 
         # With save_freq==0
         result = m.simulate_to_threshold(load, dt=2, save_freq=0)
-        self.assertListEqual(result.times, [0, 2, 4])  
+        self.assertListEqual(result.times, [0, 2, 4])
+
+        # With save_freq==0 and auto step size
+        result = m.simulate_to_threshold(load, dt=('auto', 2), save_freq=0)
+        self.assertListEqual(result.times, [0, 2, 4])
 
         result = m.simulate_to_threshold(load, dt=2, save_pts=[2.5])
         self.assertListEqual(result.times, [0, 4])  
