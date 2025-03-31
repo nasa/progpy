7da70ab7de8fc4ee7cd3796ab710d314a21bc161
Michelle Ly <michelle.b.ly@nasa.gov>
2025-03-31 12:57:31 -0700

    feat: split test_tutorials

--- tests/test_tutorials_part_two.py
+++ tests/test_tutorials_part_two.py
@@ -7,20 +7,18 @@ from testbook import testbook
 
 sys.path.append(join(dirname(__file__), ".."))
 
-class TestTutorials(unittest.TestCase):
+class TestTutorialsPartTwo(unittest.TestCase):
     def run_notebook_test(self, notebook_path):
         with testbook(notebook_path, execute=True, timeout=1200) as tb:
             self.assertEqual(tb.__class__.__name__, "TestbookNotebookClient")
         
     def test_notebook_tutorials(self):
         notebook_paths = [
-            './examples/00_Intro.ipynb',
-            './examples/01_Simulation.ipynb',
-            './examples/02_Parameter Estimation.ipynb',
-            './examples/03_Existing Models.ipynb',
-            './examples/04_New Models.ipynb',
-            './examples/05_Data Driven.ipynb',
-            './examples/06_Combining Models.ipynb'
+            './examples/07_State Estimation.ipynb',
+            './examples/08_Prediction.ipynb',
+            './examples/09_Prognostic Example.ipynb',
+            './examples/10_Prognostics Server.ipynb',
+            './examples/2024PHMTutorial.ipynb'
         ]
         for notebook_path in notebook_paths:
                 self.run_notebook_test(notebook_path)
@@ -29,7 +27,7 @@ def main():
     load_test = unittest.TestLoader()
     runner = unittest.TextTestRunner()
     print("\n\nTesting Tutorials")
-    result = runner.run(load_test.loadTestsFromTestCase(TestTutorials)).wasSuccessful()
+    result = runner.run(load_test.loadTestsFromTestCase(TestTutorialsPartTwo)).wasSuccessful()
 
     if not result:
         raise Exception("Failed test")
