d048c9e3b24aa80f3c5f241a749218a3db6f6992
Michelle Ly <lymichelle2016@icloud.com>
2025-04-25 11:10:14 -0700

    fix spacing

--- src/progpy/prognostics_model.py
+++ src/progpy/prognostics_model.py
@@ -1143,12 +1143,12 @@ class PrognosticsModel(ABC):
         
         if not saved_outputs:
             # saved_outputs is empty, so it wasn't calculated in simulation - used cached result
-            saved_outputs = LazySimResult(self.__output, saved_times, saved_states)
+            saved_outputs = LazySimResult(self.__output, saved_times, saved_states) 
             saved_event_states = LazySimResult(self.event_state, saved_times, saved_states)
         else:
             saved_outputs = SimResult(saved_times, saved_outputs, _copy=False)
             saved_event_states = SimResult(saved_times, saved_event_states, _copy=False)
-        
+
         if 'integration_method' in config:
             # Reset integration method
             self.parameters['integration_method'] = old_integration_method
