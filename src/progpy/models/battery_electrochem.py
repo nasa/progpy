b42da4e557e0c73964d01a006d89265a51f74986
Michelle Ly <lymichelle2016@icloud.com>
2025-04-29 15:37:40 -0700

    fix: remove imports

--- src/progpy/models/battery_electrochem.py
+++ src/progpy/models/battery_electrochem.py
@@ -1,10 +1,8 @@
 # Copyright © 2021 United States Government as represented by the Administrator of the
 # National Aeronautics and Space Administration.  All Rights Reserved.
 
-from copy import deepcopy
 import numpy as np
 from scipy.optimize import fsolve
-import warnings
 
 from progpy import PrognosticsModel
 
