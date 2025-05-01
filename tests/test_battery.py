a6f0c3f74fb4e28ae68eab3381a71318d6538677
Michelle Ly <lymichelle2016@icloud.com>
2025-05-01 10:35:46 -0700

    feat: add test to original model

--- tests/test_battery.py
+++ tests/test_battery.py
@@ -3,6 +3,7 @@
 from io import StringIO
 import sys
 import unittest
+import numpy as np
 
 from progpy.models import BatteryCircuit, BatteryElectroChem, BatteryElectroChemEOL, BatteryElectroChemEOD, BatteryElectroChemEODEOL, SimplifiedBattery
 from progpy.loading import Piecewise
@@ -36,11 +37,52 @@ class TestBattery(unittest.TestCase):
         result = batt.simulate_to(200, future_loading, {'t': 18.95, 'v': 4.183})
         self.assertEqual(BatteryElectroChem, BatteryElectroChemEODEOL)
 
-    # Test that the combined model has the same result if print is True or False. 
-    # This check is necessary since the event_state and output are calculated 
-    # during the simulation when print is True but only as needed 
-    # when print is False due to LazySim optimization.
-    # For more details, refer to https://github.com/nasa/progpy/issues/199
+    '''
+    Test that the combined model has the same results as the original model. The related changes can be 
+    found here: https://github.com/nasa/progpy/pull/207
+    
+    Note that states are not compared since the updated implementation uses different states and parameters.
+    '''
+    def test_battery_electrochem_results(self):
+        config = {
+            'save_freq': 1000,
+            'dt': 2,
+            'events': 'InsufficientCapacity'
+        }
+
+        def future_loading(t, x=None):
+            load = 1
+            
+            if x is not None:
+                event_state = batt.event_state(x)
+                if event_state["EOD"] > 0.95:
+                    load = 1 # Discharge
+                elif event_state["EOD"] < 0.05:
+                    load = -1 # Charge
+                    
+            return batt.InputContainer({'i': load})
+        
+        batt = BatteryElectroChem()
+        result = batt.simulate_to_threshold(future_loading, **config)
+
+        # Check a middle result
+        self.assertEqual(result.times[-10], 219000.0)
+        self.assertEqual(result.inputs[-10], {'i': np.float64(-1.0)})
+        self.assertEqual(result.outputs[-10], {'t': np.float64(18.775450787889213), 'v': np.float64(3.0165983707625728)})
+        self.assertEqual(result.event_states[-10], {'EOD': np.float64(0.16598370762572756), 'InsufficientCapacity': np.float64(0.03947368418956007)})
+
+        # Check the last result
+        self.assertEqual(result.times[-1], 228000.0)
+        self.assertEqual(result.inputs[-1], {'i': np.float64(-1.0)})
+        self.assertEqual(result.outputs[-1], {'t': np.float64(18.76922976507518), 'v': np.float64(3.0087596988706986)})
+        self.assertEqual(result.event_states[-1], {'EOD': np.float64(0.08759698870698607), 'InsufficientCapacity': 0.0})
+
+    '''
+    Test that the combined model has the same result if print is True or False. 
+    This check is necessary since the event_state and output are calculated during the simulation 
+    when print is True but only as needed when print is False due to LazySim optimization.
+    For more details, refer to https://github.com/nasa/progpy/issues/199
+    '''
     def test_battery_electrochem_printed(self):
         config = {
             'save_freq': 1000,
